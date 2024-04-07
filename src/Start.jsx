2f5c8bc0edecb2ea7c72f3bfc3aea1f36ce3190f
xilliam <78660109+realxilliam@users.noreply.github.com>
2024-04-07 19:18:34 +0300

    Buttons work now
    Yle will take a bit to load. Probably buffer (?)

--- src/Start.jsx
+++ src/Start.jsx
@@ -1,24 +1,22 @@
 import { appWindow } from '@tauri-apps/api/window';
 import ReactPlayer from 'react-player'
-import { createContext } from 'react';
+import React, { useState } from 'react';
+
+var stream = null
 
 {/* test source --> "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" */}
 
 export default function Start() {
+
+    var [stream, setStream] = React.useState(0)
+
     return (
         <div>
-            <SimpleButton label="yle" onClick={Yle} />  
-            <SimpleButton label="test 2" onClick={Test2} />
-            <Player />
+            <SimpleButton label="yle" onClick={() => setStream(stream="https://yletv.akamaized.net/hls/live/622365/yletv1fin/index.m3u8")} />  
+            <SimpleButton label="test 2" onClick={() => setStream(stream="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8")} />  
+            <Log props={stream} />
+            <ReactPlayer muted={false} playing={true} url={stream}/>
         </div>
-    )
-}
-
-function Player() {
-    var stream = Stream();
-    console.log (stream)
-    return (
-        <ReactPlayer muted={true} playing={true} url="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"/>
     )
 }
 
@@ -26,24 +24,6 @@ function SimpleButton(props) {
     return (
         <button onClick={props.onClick}>{props.label}</button>
     )
-}
-
-function Stream(props){
-    var stream = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
-    if(props == null){
-        return({stream})
-    }else{
-        stream={props}
-        console.log(stream)
-    }
-}
-
-function Yle() {
-
-}
-
-function Test2() {
-    
 }
 
 function Log(props) {
