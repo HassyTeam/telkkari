fca548d4208b3856967e3db3af0a0a838d50690d
xilliam <78660109+realxilliam@users.noreply.github.com>
2024-04-07 16:25:38 +0300

    React Player addition
    Left the variable thing there to fix, dosent work atm. Buttons also non working, there for debug

--- src/Start.jsx
+++ src/Start.jsx
@@ -1,10 +1,24 @@
 import { appWindow } from '@tauri-apps/api/window';
+import ReactPlayer from 'react-player'
+import { createContext } from 'react';
+
+{/* test source --> "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" */}
 
 export default function Start() {
     return (
         <div>
-            <SimpleButton label="test" onClick={ToggleFullscreen} />
+            <SimpleButton label="yle" onClick={Yle} />  
+            <SimpleButton label="test 2" onClick={Test2} />
+            <Player />
         </div>
+    )
+}
+
+function Player() {
+    var stream = Stream();
+    console.log (stream)
+    return (
+        <ReactPlayer muted={true} playing={true} url="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"/>
     )
 }
 
@@ -14,6 +28,24 @@ function SimpleButton(props) {
     )
 }
 
-function ToggleFullscreen() {
+function Stream(props){
+    var stream = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+    if(props == null){
+        return({stream})
+    }else{
+        stream={props}
+        console.log(stream)
+    }
+}
+
+function Yle() {
+
+}
+
+function Test2() {
     
+}
+
+function Log(props) {
+    console.log({props})
 }
