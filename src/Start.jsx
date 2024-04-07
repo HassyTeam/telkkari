6c9f06cd554a84690972d5f134051df0b2e11c5f
xilliam <78660109+realxilliam@users.noreply.github.com>
2024-04-07 12:56:47 +0300

    SimpleButton component and tauri api added

--- src/Start.jsx
+++ src/Start.jsx
@@ -1,7 +1,19 @@
+import { appWindow } from '@tauri-apps/api/window';
+
 export default function Start() {
     return (
         <div>
-            <p>moi :)</p>
+            <SimpleButton label="test" onClick={ToggleFullscreen} />
         </div>
     )
+}
+
+function SimpleButton(props) {
+    return (
+        <button onClick={props.onClick}>{props.label}</button>
+    )
+}
+
+function ToggleFullscreen() {
+    
 }
