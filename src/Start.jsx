43591683169cd6b132ce1a5a609f1393a0b7a724
xilliam <78660109+realxilliam@users.noreply.github.com>
2024-04-08 21:37:22 +0300

    Revert "Revert "Revert "Made player fullscreen and more"""
    This reverts commit 017ac7acf35359a4c2664f60fcd55551aaed7ed7.

--- src/Start.jsx
+++ src/Start.jsx
@@ -2,21 +2,20 @@ import { appWindow } from '@tauri-apps/api/window';
 import ReactPlayer from 'react-player'
 import React, { useState } from 'react';
 
-var stream = null;
+var stream = null
 
 {/* test source --> "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" */}
 
 export default function Start() {
 
-    var [stream, setStream] = React.useState("https://yletv.akamaized.net/hls/live/622365/yletv1fin/index.m3u8")
+    var [stream, setStream] = React.useState(0)
 
     return (
-        <div className='player-wrapper'>
-            {/* debug buttons */}
-            {/* <SimpleButton label="yle" onClick={() => setStream(stream="https://yletv.akamaized.net/hls/live/622365/yletv1fin/index.m3u8")} />  
-            <SimpleButton label="test 2" onClick={() => setStream(stream="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8")} /> */}
+        <div>
+            <SimpleButton label="yle" onClick={() => setStream(stream="https://yletv.akamaized.net/hls/live/622365/yletv1fin/index.m3u8")} />  
+            <SimpleButton label="test 2" onClick={() => setStream(stream="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8")} />  
             <Log props={stream} />
-            <ReactPlayer className="react-player" width="100%" height="100vh" muted={false} playing={true} url={stream}/>
+            <ReactPlayer muted={false} playing={true} url={stream}/>
         </div>
     )
 }
