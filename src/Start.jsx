8358a1c19991f70af90c66fff83a5f53a613e389
xilliam <78660109+realxilliam@users.noreply.github.com>
2024-04-08 21:39:50 +0300

    Made player fullscreen and more
    Made background black
    Made dark mode defaults defaults on light mode default too
    Commented debug buttons

--- src/Start.jsx
+++ src/Start.jsx
@@ -2,20 +2,21 @@ import { appWindow } from '@tauri-apps/api/window';
 import ReactPlayer from 'react-player'
 import React, { useState } from 'react';
 
-var stream = null
+var stream = null;
 
 {/* test source --> "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" */}
 
 export default function Start() {
 
-    var [stream, setStream] = React.useState(0)
+    var [stream, setStream] = React.useState("https://yletv.akamaized.net/hls/live/622365/yletv1fin/index.m3u8")
 
     return (
-        <div>
-            <SimpleButton label="yle" onClick={() => setStream(stream="https://yletv.akamaized.net/hls/live/622365/yletv1fin/index.m3u8")} />  
-            <SimpleButton label="test 2" onClick={() => setStream(stream="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8")} />  
+        <div className='player-wrapper'>
+            {/* debug buttons */}
+            {/* <SimpleButton label="yle" onClick={() => setStream(stream="https://yletv.akamaized.net/hls/live/622365/yletv1fin/index.m3u8")} />  
+            <SimpleButton label="test 2" onClick={() => setStream(stream="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8")} /> */}
             <Log props={stream} />
-            <ReactPlayer muted={false} playing={true} url={stream}/>
+            <ReactPlayer className="react-player" width="100%" height="100vh" muted={false} playing={true} url={stream}/>
         </div>
     )
 }
