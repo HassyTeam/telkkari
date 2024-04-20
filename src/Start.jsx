d5cd5c6adbe23f9b434b497204cdd880224bd541
xilliam <78660109+realxilliam@users.noreply.github.com>
2024-04-20 19:21:38 +0300

    Started work on passing json data to map func

--- src/Start.jsx
+++ src/Start.jsx
@@ -3,7 +3,6 @@ import ReactPlayer from 'react-player'
 import React, { useState } from 'react';
 import json from "./confs/playlists.json"
 
-var json_playlists = json
 
 {/* test source --> "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" */}
 
@@ -17,53 +16,35 @@ export default function Start() {
             {/* <SimpleButton label="yle" onClick={() => setStream(stream="https://yletv.akamaized.net/hls/live/622365/yletv1fin/index.m3u8")} />  
             <SimpleButton label="test 2" onClick={() => setStream(stream="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8")} /> */}
             {/* <Log props={stream} /> */}
-            <ReactPlayer className="react-player" width="100%" height="100vh" muted={false} playing={true} url={stream}/>
-            <Sidebar playlists={json_playlists} />
+            <ReactPlayer className="react-player" width="100%" height="100vh" muted={true} playing={true} url={stream}/>
+            <Sidebar />
         </div>
     )
 }
 
-function Sidebar({ json_playlists }) {
-    for (var prop in json_playlists) {
-        //console.log("Prop: " + prop)
-        //console.log("Key:" + prop);
-        var id = prop
-        var name = json_playlists[prop]["name"]
-        var propstream = json_playlists[prop]["stream"]
-        //console.log(propstream)
-        //console.log("Value:" + value);
-        setPlaylistsFunc(args={ "id": id, "name": "'"+name+"'", "stream": "'" + propstream + "'" })
-    }
+function Sidebar() {
+    //const [playlists, setPlaylists] = useState([])
+    
 
-    console.log(setPlaylistsFunc(query = "return"))
     return (
-      <section className='sidebar'>
-        {/* Not working yet, expected behaviour 
-        {channels.map(channel =>
-          <NewChannel channel={value} />
-        )}
-        */}
-      </section>
+        <section className='sidebar'>
+            <h1>Channels</h1>
+            <>
+            {json.map(function(data) {
+                return (
+                    <h1>{data}</h1>
+                )
+            })}
+            </>
+        </section>
     );
 }
 
-
-// !!IMPORTANT!! This is a standard JS function, dont use props.args, just use args
-function setPlaylistsFunc({props}) {
-    const [playlists, setPlaylists] = useState([])
-
-    if({props.query}="return"){
-        return(
-            playlists
-        )
-    } else{
-        setPlaylists([...playlists, {props.args} ])
-    }
-}
+// why did we choose rust for this project
 
 function NewChannel({ props }) {
 return (
-    <p>{props.id}</p>
+    <p>{props.playlist}</p>
 );
 }
 
